b849cf2714a3225ce0762370cc35527caf10af5a
RakuJa <danielegiachetto1999@gmail.com>
2020-12-04 18:37:45 +0100

    close #6

--- src/main/java/it/unipd/tos/business/TakeAwayBill.java
+++ src/main/java/it/unipd/tos/business/TakeAwayBill.java
@@ -4,12 +4,15 @@
 
 
 package it.unipd.tos.business;
+import java.time.LocalTime;
 import java.util.List;
+
 import it.unipd.tos.business.exception.RestaurantBillException;
 import it.unipd.tos.model.MenuItem;
 import it.unipd.tos.model.User;
 public interface TakeAwayBill {
-    double getOrderPrice(List<MenuItem> itemsOrdered, User user) 
+    double getOrderPrice(List<MenuItem> itemsOrdered, User user,
+            LocalTime time) 
             throws RestaurantBillException;
 }
     
